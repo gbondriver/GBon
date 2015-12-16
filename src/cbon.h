85e7049720a72b4859e13032f1d99de7e5542657
Iga Gari <igari.y@gmail.com>
2015-12-16 14:12:15 +0900

    fixed set_channel()

--- src/cbon.h
+++ src/cbon.h
@@ -16,7 +16,8 @@ EXTERNC void cbon_close_module(CBon *cbon);
 
 EXTERNC int cbon_create_driver(CBon *cbon);
 EXTERNC int cbon_open_tuner(CBon *cbon);
-EXTERNC int cbon_set_channel(CBon *cbon, unsigned char space, unsigned char ch);
+EXTERNC int cbon_set_channel(CBon *cbon, unsigned char ch);
+EXTERNC int cbon_set_channel2(CBon *cbon, unsigned char space, unsigned char ch);
 EXTERNC int cbon_get_ts_stream(CBon *cbon, unsigned char **dest,
                                unsigned int *size, unsigned int *remain);
 EXTERNC void cbon_close_tuner(CBon *cbon);
