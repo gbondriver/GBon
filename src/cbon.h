d15c3a449dcaa2e313e387601db5cc3465dc130b
Iga Gari <igari.y@gmail.com>
2015-12-21 15:24:07 +0900

    change extern "C"

--- src/cbon.h
+++ src/cbon.h
@@ -1,26 +1,27 @@
 #ifdef __cplusplus
-#define EXTERNC extern "C"
-#else
-#define EXTERNC
+extern "C" {
 #endif
 
-EXTERNC typedef struct _CBon {
+typedef struct _CBon {
     void *hModule;
     void *pIBon;
     void *pIBon2;
     void *pIBon3;
 }CBon;
 
-EXTERNC CBon *cbon_load_module(char *driverFilename);
-EXTERNC void cbon_close_module(CBon *cbon);
+CBon *cbon_load_module(char *driverFilename);
+void cbon_close_module(CBon *cbon);
 
-EXTERNC int cbon_create_driver(CBon *cbon);
-EXTERNC int cbon_open_tuner(CBon *cbon);
-EXTERNC int cbon_set_channel(CBon *cbon, unsigned char ch);
-EXTERNC int cbon_set_channel2(CBon *cbon, unsigned char space, unsigned char ch);
-EXTERNC int cbon_get_ts_stream(CBon *cbon, unsigned char **dest,
-                               unsigned int *size, unsigned int *remain);
-EXTERNC void cbon_close_tuner(CBon *cbon);
-EXTERNC void cbon_release(CBon *cbon);
+int cbon_create_driver(CBon *cbon);
+int cbon_open_tuner(CBon *cbon);
+int cbon_set_channel(CBon *cbon, unsigned char ch);
+int cbon_set_channel2(CBon *cbon, unsigned char space, unsigned char ch);
+int cbon_get_ts_stream(CBon *cbon, unsigned char **dest,
+                        unsigned int *size, unsigned int *remain);
+void cbon_close_tuner(CBon *cbon);
+void cbon_release(CBon *cbon);
 
 
+#ifdef __cplusplus
+}
+#endif
